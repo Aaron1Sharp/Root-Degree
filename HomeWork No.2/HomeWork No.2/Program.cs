5dc64600781ee20a8e000f1d490789ef123ffc49
Aaron1sharp <podaroktebe2014@gmail.com>
2020-01-06 21:20:35 +0600

    variables

--- HomeWork No.2/HomeWork No.2/Program.cs
+++ HomeWork No.2/HomeWork No.2/Program.cs
@@ -20,21 +20,21 @@ namespace homework_No._2
         ///     Math.Abs = Возвращает абсолютное значение указанного числа.
         ///     Math.Pow = Возвращает указанное число, возведенное в указанную степень.
         /// </summary>
-        /// <param name="n"></param>
-        /// <param name="A"></param>
+        /// <param name="degree"></param>
+        /// <param name="radical"></param>
         /// <param name="eps"></param>
         /// <returns>
         ///     SqrtN = 1,55184557460721
         /// </returns>
-        static public double SqrtN(double n, double A, double eps = 0.0001)
+        static public double SqrtN(double degree, double radical, double eps = 0.0001)
         { 
-            var x0 = A / n;
-            var x1 = (1 / n) * ((n - 1) * x0 + A / Math.Pow(x0, (int)n - 1));
+            var x0 = radical / degree;
+            var x1 = (1 / degree) * ((degree - 1) * x0 + radical / Math.Pow(x0, (int)degree - 1));
 
             while (Math.Abs(x1 - x0) > eps)
             {
                 x0 = x1;
-                x1 = (1 / n) * ((n - 1) * x0 + A / Math.Pow(x0, (int)n - 1));
+                x1 = (1 / degree) * ((degree - 1) * x0 + radical / Math.Pow(x0, (int)degree - 1));
             }
             return x1;
 
