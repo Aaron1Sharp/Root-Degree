c94bc72f0d8a8905fe1b575fe7d8c49cc9547768
Aaron1sharp <podaroktebe2014@gmail.com>
2020-01-07 23:16:07 +0600

    newprogram

---
+++ Convert decimal to binary/Convert decimal to binary/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Convert_decimal_to_binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SystemMethod(4));
+            Console.ReadKey();
+        }
+
+        static public int SystemMethod(int numberdecimal)
+        {
+            int count = 1;
+            Console.WriteLine(numberdecimal);
+            for (; numberdecimal > 0; numberdecimal /= 2, ++count)
+            {
+                Console.WriteLine();
+                if ((numberdecimal % 2) != 0)
+                {
+                    Console.Write(" 1** =");
+                }
+                else
+                {
+                    Console.Write(" 0** =");
+                }
+                Console.Write(" цикл-- " + count + " --= ");
+            }
+            return numberdecimal;
+        }
+    }
+}
