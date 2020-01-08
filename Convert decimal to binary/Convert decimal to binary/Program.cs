e140ac51fbf31e862aed66566fb2cacd7f8314ab
Aaron1sharp <podaroktebe2014@gmail.com>
2020-01-08 23:48:50 +0600

    XML

--- Convert decimal to binary/Convert decimal to binary/Program.cs
+++ Convert decimal to binary/Convert decimal to binary/Program.cs
@@ -6,13 +6,13 @@ namespace Convert_decimal_to_binary
     class Program
     {
         /// <summary>
-        /// Вводим число и на выводе получаем его в двоичной системеа
+        /// Вводим число и на выводе получаем его в двоичной системе
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
             int numberdecimal;
-            Console.WriteLine("ввод числа");
+            Console.WriteLine("введите число");
             numberdecimal = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine(Convert.ToString(BinaryMethod.SystemMethod(numberdecimal)) + " = в двоичной системе");
             Console.ReadKey();
