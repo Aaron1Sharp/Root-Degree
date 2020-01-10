a1e7861dfc54856fc866c794dde9b2cbf14dc00c
Aaron1sharp <podaroktebe2014@gmail.com>
2020-01-11 03:34:57 +0600

    перегрузка

---
+++ FormAlgorithmEuclidean/FormAlgorithmEuclidean/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace FormAlgorithmEuclidean
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.button1.Location = new System.Drawing.Point(247, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Высчитать НОД";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F);
+            this.label1.Location = new System.Drawing.Point(256, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "= ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F);
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(12, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(229, 46);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F);
+            this.textBox2.ForeColor = System.Drawing.Color.Black;
+            this.textBox2.Location = new System.Drawing.Point(12, 57);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(229, 46);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F);
+            this.textBox3.ForeColor = System.Drawing.Color.Black;
+            this.textBox3.Location = new System.Drawing.Point(12, 109);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(229, 46);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F);
+            this.textBox4.ForeColor = System.Drawing.Color.Black;
+            this.textBox4.Location = new System.Drawing.Point(12, 161);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(229, 46);
+            this.textBox4.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 217);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ FormAlgorithmEuclidean/FormAlgorithmEuclidean/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormAlgorithmEuclidean
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // ВСЕ ЧИСЛА В КОММЕНТАРИЯХ ВЫМЫШЛЕННЫЕ
+                // AL = AlgorithmEuclidean
+                // EM = EuclideanMethod
+                // AL.EM(AL.EM(16, 36), AL.EM(585, 360));
+                if (!string.IsNullOrWhiteSpace(textBox3.Text) && !string.IsNullOrWhiteSpace(textBox4.Text))
+                {
+                    label1.Text = Convert.ToString(AlgorithmEuclidean.EuclideanMethod(AlgorithmEuclidean.EuclideanMethod(Convert.ToInt32(textBox1.Text),
+                     Convert.ToInt32(textBox2.Text)), AlgorithmEuclidean.EuclideanMethod(Convert.ToInt32(textBox3.Text), Convert.ToInt32(textBox4.Text))) + " = НОД из 4");
+                }
+                // AL.EM(36,60);
+                else if (string.IsNullOrWhiteSpace(textBox3.Text) && string.IsNullOrWhiteSpace(textBox4.Text))
+                {
+                    label1.Text = Convert.ToString(AlgorithmEuclidean.EuclideanMethod(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text)) + " = НОД из 2");
+                }
+                // AL.EM(AL.EM(15, 30), 75);
+                else if (string.IsNullOrWhiteSpace(textBox4.Text))
+                {
+                    label1.Text = Convert.ToString(AlgorithmEuclidean.EuclideanMethod(AlgorithmEuclidean.EuclideanMethod(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text)), Convert.ToInt32(textBox3.Text)) + " = НОД из 3");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Введите положительные числа по порядку минимум в первые два столбика");
+            }
+
+        }
+        private void TextBox2_TextChanged(object sender, EventArgs e) { }
+        private void TextBox1_TextChanged(object sender, EventArgs e) { }
+    }
+}
+
