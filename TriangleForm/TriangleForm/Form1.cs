6548dc5a14860b314d74565dc5b30c1db1ad7fd1
Aaron1sharp <podaroktebe2014@gmail.com>
2020-01-12 19:41:13 +0600

    Form

---
+++ TriangleForm/TriangleForm/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TriangleForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double A, B, C, p, S;
+                A = double.Parse(textBox1.Text);
+                B = double.Parse(textBox2.Text);
+                C = double.Parse(textBox3.Text);
+                p = (A + B + C) / 2;
+
+                S = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+                label1.Text = S.ToString();
+                label2.Text = p.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Укажите все стороны треугольника");
+            }
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)  { }
+
+        private void Label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
